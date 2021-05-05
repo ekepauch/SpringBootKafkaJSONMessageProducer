38f5781c4baaf24c0219cc7809c5d2d45b9c5028
Davyjones <ekenedavidson15@gmail.com>
2021-05-05 20:47:39 +0100

    INITIAL COMMIT

---
+++ src/main/java/com/onlinetutorialspoint/HelloController.java
@@ -0,0 +1,23 @@
+package com.onlinetutorialspoint;
+
+import com.onlinetutorialspoint.model.Item;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("producer")
+public class HelloController {
+
+    @Autowired
+    KafkaTemplate<String, Item> KafkaJsontemplate;
+    String TOPIC_NAME = "items-topic";
+    String KEY = "2";
+
+    @PostMapping(value = "/postItem",consumes = {"application/json"},produces = {"application/json"})
+    public String postJsonMessage(@RequestBody Item item){
+//        KafkaJsontemplate.send(TOPIC_NAME,new Item(1,"Lenovo","Laptop"));
+        KafkaJsontemplate.send(TOPIC_NAME, item);
+        return "Message published successfully";
+    }
+}
